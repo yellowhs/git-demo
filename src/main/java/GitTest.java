35ac936f9b6eaf356711c57adcc254e7fe282a76
huangshuai <huangshuai@163.com>
2023-07-02 21:05:38 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: huangshuai
+ * @Date: 2023/7/2 20:58
+ * @Description：
+ * @Version：
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test demo");
+    }
+}
